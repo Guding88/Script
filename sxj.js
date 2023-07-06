cafbee08ed83479c5c0a06c571de573937937daf
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-06 16:20:33 +0800

    Update sxj.js

--- sxj.js
+++ sxj.js
@@ -2,6 +2,12 @@
 
 水心记：https://apps.apple.com/app/id1581076145
 
+[rewrite_local]
+^https?:\/\/api\.revenuecat\.com\/v1\/subscribers\/[^\/]+$ url script-response-body https://raw.githubusercontent.com/Guding88/Scritp/main/sxj.js
+
+[MITM]
+hostname = api.revenuecat.com
+
 */
 
 var guding = JSON.parse($response.body);
