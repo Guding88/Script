2a45c005176073b89e0a1a7dc75c403455b03d48
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-06 16:19:13 +0800

    Update cpp.js

--- cpp.js
+++ cpp.js
@@ -2,6 +2,12 @@
 
 车票票：https://apps.apple.com/app/id6446212291
 
+[rewrite_local]
+^https?:\/\/api\.revenuecat\.com\/v1\/subscribers\/[^\/]+$ url script-response-body https://raw.githubusercontent.com/Guding88/Scritp/main/cpp.js
+
+[MITM]
+hostname = api.revenuecat.com
+
 */
 
 var guding = JSON.parse($response.body);
