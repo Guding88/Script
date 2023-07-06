60b808e477fe8f7a029a474e5826b0dea7fe2435
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-06 12:42:08 +0800

    Create NFC.js

---
+++ NFC.js
@@ -0,0 +1,94 @@
+/*
+
+NFC Scanner Tool for iPhone:https://apps.apple.com/app/id1249686798
+
+*/
+
+var guding = JSON.parse($response.body);
+guding.data["permissions"] = [{
+    "id": "permission_webhook",
+    "active": 1,
+    "renew_state": 1,
+    "associated_product": "unlimited_yearly",
+}, {
+    "id": "permission_unlimited_pdf_pages",
+    "active": 1,
+    "renew_state": 1,
+    "associated_product": "unlimited_yearly",
+}, {
+    "id": "permission_unlimited_qr_code_widgets",
+    "active": 1,
+    "renew_state": 1,
+    "associated_product": "unlimited_yearly",
+}, {
+    "id": "permission_ad_free",
+    "active": 1,
+    "renew_state": 1,
+    "associated_product": "unlimited_yearly",
+}, {
+    "id": "permission_appicon_dark_rainbow",
+    "active": 1,
+    "renew_state": 1,
+    "associated_product": "unlimited_yearly",
+}, {
+    "id": "permission_appicon_gold",
+    "active": 1,
+    "renew_state": 1,
+    "associated_product": "unlimited_yearly",
+}, {
+    "id": "permission_appicon_hackergreen",
+    "active": 1,
+    "renew_state": 1,
+    "associated_product": "unlimited_yearly",
+}, {
+    "id": "permission_appicon_rainbow",
+    "active": 1,
+    "renew_state": 1,
+    "associated_product": "unlimited_yearly",
+}, {
+    "id": "permission_appicon_rastafari",
+    "active": 1,
+    "renew_state": 1,
+    "associated_product": "unlimited_yearly", 
+}, {
+    "id": "permission_appicon_ruby",
+    "active": 1,
+    "renew_state": 1,
+    "associated_product": "unlimited_yearly", 
+}, {
+    "id": "permission_speech_synthesizer",
+    "active": 1,
+    "renew_state": 1,
+    "associated_product": "unlimited_yearly",
+}, {
+    "id": "permission_theme_englishgreen",
+    "active": 1,
+    "renew_state": 1,
+    "associated_product": "unlimited_yearly",
+}, {
+    "id": "permission_theme_gold",
+    "active": 1,
+    "renew_state": 1,
+    "associated_product": "unlimited_yearly",
+}, {
+    "id": "permission_theme_hackergreen",
+    "active": 1,
+    "renew_state": 1,
+    "associated_product": "unlimited_yearly",
+}, {
+    "id": "permission_theme_horsered",
+    "active": 1,
+    "renew_state": 1,
+    "associated_product": "unlimited_yearly",
+}, {
+    "id": "permission_theme_purplehaze",
+    "active": 1,
+    "renew_state": 1,
+    "associated_product": "unlimited_yearly",
+}, {
+    "id": "permission_unlimited",
+    "active": 1,
+    "renew_state": 1,
+    "associated_product": "unlimited_yearly",
+}]
+$done({body: JSON.stringify(guding)});
