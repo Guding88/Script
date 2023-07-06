554b64ee1132c7fc4d81b777aec795c9c9ed90d0
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-06 16:42:42 +0800

    Create xgsrf.js

---
+++ xgsrf.js
@@ -0,0 +1,78 @@
+/*
+
+小戈输入法：
+
+[rewrite_local]
+^https?:\/\/api\.boyasec\.com\/ime\/rights\/list url script-response-body https://raw.githubusercontent.com/Guding88/Scritp/main/xgsrf.js
+
+[MITM]
+hostname = api.boyasec.com
+
+*/
+
+var guding = JSON.parse($response.body);
+
+guding.data = [
+    {
+      "name" : "应用安全锁",
+      "expiration" : 148204937166000,
+      "code" : "SecurityLock"
+    },
+    {
+      "name" : "隐文风格",
+      "expiration" : 148204937166000,
+      "code" : "Cryptic"
+    },
+    {
+      "name" : "更换图标",
+      "expiration" : 148204937166000,
+      "code" : "ChangeIcon"
+    },
+    {
+      "name" : "文件加密",
+      "expiration" : 148204937166000,
+      "code" : "FileEncrypt"
+    },
+    {
+      "name" : "加密通话",
+      "expiration" : 148204937166000,
+      "code" : "CallEncrypt"
+    },
+    {
+      "name" : "私密发送",
+      "expiration" : 148204937166000,
+      "code" : "Private"
+    },
+    {
+      "name" : "全能慧聊",
+      "expiration" : 148204937166000,
+      "code" : "ChatGPT"
+    },
+    {
+      "name" : "全时撤回",
+      "expiration" : 148204937166000,
+      "code" : "Retract"
+    },
+    {
+      "name" : "阅后即焚",
+      "expiration" : 148204937166000,
+      "code" : "Snapchat"
+    },
+    {
+      "name" : "语音加密",
+      "expiration" : 148204937166000,
+      "code" : "VoiceEncrypt"
+    },
+    {
+      "name" : "通话设置",
+      "expiration" : 148204937166000,
+      "code" : "CallConfig"
+    },
+    {
+      "name" : "文件密库",
+      "expiration" : 148204937166000,
+      "code" : "FileVault"
+    }
+  ];
+
+$done({ body: JSON.stringify(guding) });
