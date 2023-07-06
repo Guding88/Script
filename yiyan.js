362a3498e9090913aff50ea61e0038091965e6d5
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-06 17:22:59 +0800

    Create yiyan.js

---
+++ yiyan.js
@@ -0,0 +1,12 @@
+/*
+
+[rewrite_local]
+^https?:\/\/app\.yiyan\.art\/yiyan url script-response-body https://raw.githubusercontent.com/Guding88/Scritp/main/yiyan.js
+
+[MITM]
+hostname = app.yiyan.art
+
+*/
+var guding = JSON.parse($response.body);
+guding = JSON.stringify(guding).replace(/"viptype":"\d+"/g, '"viptype":"4"');
+$done({ body: guding });
