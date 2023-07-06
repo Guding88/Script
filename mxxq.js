695f29e84d7c25edddadd9a9ff8fe836b51e1759
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-06 16:25:38 +0800

    Update mxxq.js

--- mxxq.js
+++ mxxq.js
@@ -3,6 +3,12 @@
 冥想星球：https://apps.apple.com/app/id1472457967
 解锁冥想模块，声音模块可在冥想的背景音乐中使用，计划模块不可用。
 
+[rewrite_local]
+^https:\/\/kc\.xinli001\.com\/lingxikc\/(?!.*plan).* url script-response-body https://raw.githubusercontent.com/Guding88/Scritp/main/mxxq.js
+
+[MITM]
+hostname = kc.xinli001.com
+
 */
 var guding = $response.body;
 guding  = guding.replace(/"isJoin":\w+/g, '"isJoin":true');
