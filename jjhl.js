135f93096440d4d5b9520d1ec3f623298474962d
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-06 16:19:49 +0800

    Update jjhl.js

--- jjhl.js
+++ jjhl.js
@@ -2,6 +2,12 @@
 
 极简汇率
 
+[rewrite_local]
+^https?:\/\/api\.revenuecat\.com\/v1\/subscribers\/[^\/]+$ url script-response-body https://raw.githubusercontent.com/Guding88/Scritp/main/jjhl.js
+
+[MITM]
+hostname = api.revenuecat.com
+
 */
 
 var guding = JSON.parse($response.body);
