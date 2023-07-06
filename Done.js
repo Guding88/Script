9278f99cd56f1e91f6e80bc084bff7fdb36db40c
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-06 12:37:57 +0800

    Update Done.js

--- Done.js
+++ Done.js
@@ -1,13 +1,13 @@
 var guding = JSON.parse($response.body);
 guding.subscriber.entitlements = {
   "Premium": {
-    "expires_date": "2333-02-23T00:02:33Z",
+    "expires_date": "6666-06-06T06:06:06Z",
     "product_identifier": "com.tbd.Done.growth.bundle.yearly.v6",
   }
 };
 guding.subscriber.subscriptions = {
   "com.tbd.Done.growth.bundle.yearly.v6":{
-    "expires_date":"2333-02-23T00:02:33Z",
+    "expires_date":"6666-06-06T06:06:06Z",
   }
 };
 
