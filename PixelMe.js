ca0fa7bd90c040e9c6243dc44b1b6e83d6aff092
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-06 21:59:43 +0800

    Update PixelMe.js

--- PixelMe.js
+++ PixelMe.js
@@ -1,7 +1,7 @@
 /*
 
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com\/v1\/subscribers\/[^\/]+$ url script-response-body https://raw.githubusercontent.com/Guding88/Scritp/main/PixelMe.js
+^https?:\/\/api\.revenuecat\.com\/v1\/subscribers\/[^\/]+$ url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/PixelMe.js
 
 [MITM]
 hostname = api.revenuecat.com
