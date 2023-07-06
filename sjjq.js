3c0767df9f8d2c594762599e108842f55e721356
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-06 22:00:55 +0800

    Update sjjq.js

--- sjjq.js
+++ sjjq.js
@@ -3,7 +3,7 @@
 时间机器：https://apps.apple.com/app/id1502507360
 
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com\/v1\/subscribers\/[^\/]+$ url script-response-body https://raw.githubusercontent.com/Guding88/Scritp/main/sjjq.js
+^https?:\/\/api\.revenuecat\.com\/v1\/subscribers\/[^\/]+$ url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/sjjq.js
 
 [MITM]
 hostname = api.revenuecat.com
