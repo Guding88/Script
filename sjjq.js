554d19ac52a3664c75f5c576db794f95e075815a
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-06 14:47:10 +0800

    Create sjjq.js

---
+++ sjjq.js
@@ -0,0 +1,26 @@
+/*
+
+时间机器：https://apps.apple.com/app/id1502507360
+
+*/
+
+
+var guding = JSON.parse($response.body);
+guding.subscriber.entitlements = {
+  "timemachine_pro": {
+    "expires_date": "6666-06-06T06:06:06Z",
+    "product_identifier": "com.abouttime.timemachine.year",
+    "purchase_date": "2023-02-23T02:33:33Z"
+  }
+};
+guding.subscriber.original_purchase_date = "2023-02-23T03:33:33Z";
+guding.subscriber.subscriptions = {
+  "com.abouttime.timemachine.year": {
+    "expires_date": "6666-06-06T06:06:06Z",
+    "original_purchase_date": "2023-02-23T02:33:33Z",
+    "purchase_date": "2023-02-23T02:33:33Z",
+    "ownership_type" : "PURCHASED",
+    "store" : "app_store"
+  }
+};
+$done({ body: JSON.stringify(guding) });
