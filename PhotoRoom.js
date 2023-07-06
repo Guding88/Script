ace63cd41c9af99c06467656b2e38a9924cb8b9d
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-06 16:17:24 +0800

    Update PhotoRoom.js

--- PhotoRoom.js
+++ PhotoRoom.js
@@ -2,6 +2,12 @@
 
 PhotoRoom：https://apps.apple.com/app/id1455009060
 
+[rewrite_local]
+^https?:\/\/api\.revenuecat\.com\/v1\/subscribers\/[^\/]+$ url script-response-body https://raw.githubusercontent.com/Guding88/Scritp/main/PhotoRoom.js
+
+[MITM]
+hostname = api.revenuecat.com
+
 */
 
 var guding = JSON.parse($response.body);
