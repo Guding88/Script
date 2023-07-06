83500116dc6f35a452399600674c2cd6afd3ec6b
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-06 20:52:11 +0800

    Create Authenticator.js

---
+++ Authenticator.js
@@ -0,0 +1,32 @@
+/*
+
+Authenticator：https://apps.apple.com/app/id1538761576
+
+[rewrite_local]
+^https?:\/\/api\.revenuecat\.com\/v1\/subscribers\/[^\/]+$ url script-response-body https://raw.githubusercontent.com/Guding88/Scritp/main/Authenticator.js
+
+[MITM]
+hostname = api.revenuecat.com
+
+*/
+
+var guding = JSON.parse($response.body);
+guding.subscriber.entitlements = {
+  "PRO": {
+    "expires_date": "6666-06-06T06:06:06Z",
+    "product_identifier": "2fa_0499_1y",
+    "purchase_date": "2023-02-23T02:33:33Z"
+  }
+};
+guding.subscriber.original_purchase_date = "2023-02-23T03:33:33Z";
+
+guding.subscriber.subscriptions = {
+  "2fa_0499_1y": {
+    "expires_date": "6666-06-06T06:06:06Z",
+    "original_purchase_date": "2023-02-23T02:33:33Z",
+    "purchase_date": "2023-02-23T02:33:33Z",
+    "ownership_type" : "PURCHASED",
+    "store" : "app_store"
+  }
+};
+$done({ body: JSON.stringify(guding) });
