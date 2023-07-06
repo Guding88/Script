72f04dff2988a461fa63658b81dfac135ec39849
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-06 15:26:46 +0800

    Update Cubox.js

--- Cubox.js
+++ Cubox.js
@@ -1,3 +1,13 @@
+/*
+
+[rewrite_local]
+^https:\/\/kc\.xinli001\.com\/lingxikc\/meditating\/api\/(?!.*plan).* url script-response-body ttps://raw.githubusercontent.com/Guding88/Screit/Cubox.js
+
+[MITM]
+hostname = kc.xinli001.com
+
+*/
+
 var guding = JSON.parse($response.body);
 guding.data.isExpire = false;
 guding.data.expireTime ="6666-06-06T06:06:06Z";
