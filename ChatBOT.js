b835725ad0671e0a62afb7e0ebe4d6d8e8d4754d
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-07 22:27:12 +0800

    Create ChatBOT.js

---
+++ ChatBOT.js
@@ -0,0 +1,15 @@
+/*
+
+ChatBOT：https://apps.apple.com/app/id6448030649
+
+[rewrite_local]
+^https?:\/\/greatios\.xitengtech\.cn\/api\/visitors\/sign-in url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/ChatBOT.js
+
+[MITM]
+hostname = buy.itunes.apple.com
+
+*/
+var guding = $response.body;
+guding = guding.replace(/"vip":\d+/g, '"vip":1');
+guding = guding.replace(/"vip_expire_time":\d+/g, '"vip_expire_time":"148204937166000"');
+$done({ body:guding});
