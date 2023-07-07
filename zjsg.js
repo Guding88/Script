877cfd28ff641253f3facaa24cddbd3f11fff9ba
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-07 11:08:48 +0800

    Create zjsg.js

---
+++ zjsg.js
@@ -0,0 +1,14 @@
+/*
+
+指尖时光：https://apps.apple.com/app/id1392166974
+
+[rewrite_local]
+^https?:\/\/integral2\.dasyibalang\.com/v2\/User url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/zjsg.js
+
+[MITM]
+hostname = api.qonversion.io
+
+*/
+var guding = $response.body;
+guding  = guding.replace(/"VIP":\d+/g, '"VIP":1');
+$done({ body:guding});
