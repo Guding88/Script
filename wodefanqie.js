a4028c338c1f15aa5df68605cda836740938adb5
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-09 11:43:58 +0800

    更新 wodefanqie.js

--- wodefanqie.js
+++ wodefanqie.js
@@ -1,1 +1,31 @@
+/*
 
+我的番茄：https://apps.apple.com/app/id1528322796
+
+[rewrite_local]
+^https?:\/\/api\.revenuecat\.com\/v1\/subscribers\/[^\/]+$|^https:\/\/api\.revenuecat\.com\/v1\/receipts$ url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/wodefanqie.js
+
+[MITM]
+hostname = api.revenuecat.com
+
+*/
+var guding = JSON.parse($response.body);
+guding.subscriber.entitlements = {
+  "Plus": {
+    "expires_date": "6666-06-06T06:06:06Z",
+    "product_identifier": "com.MINE.PomodoroTimer.plus.lifetime",
+    "purchase_date": "2023-02-23T02:33:33Z"
+  }
+};
+guding.subscriber.original_purchase_date = "2023-02-23T03:33:33Z";
+
+guding.subscriber.subscriptions = {
+  "com.MINE.PomodoroTimer.plus.lifetime": {
+    "expires_date": "6666-06-06T06:06:06Z",
+    "original_purchase_date": "2023-02-23T02:33:33Z",
+    "purchase_date": "2023-02-23T02:33:33Z",
+    "ownership_type" : "PURCHASED",
+    "store" : "app_store"
+  }
+};
+$done({ body: JSON.stringify(guding) });
