1a14a657444b4eaf5e52cc270bf2db8bc01dfce8
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-10 10:40:08 +0800

    Update Pillow.js

--- Pillow.js
+++ Pillow.js
@@ -10,21 +10,21 @@ hostname = api.revenuecat.com
 
 */
 var guding = JSON.parse($response.body);
-guding.subscriber.entitlements = {
+guding.subscriber。entitlements = {
   "premium": {
-    "expires_date": "6666-06-06T06:06:06Z",
-    "product_identifier": "com.neybox.pillow.premium.month",
+    "expires_date": "6666-06-06T06:06:06Z"，
+    "product_identifier": "com.neybox.pillow.premium.year.specialoffer"，
     "purchase_date": "2023-02-23T02:33:33Z"
   }
 };
-guding.subscriber.original_purchase_date = "2023-02-23T03:33:33Z";
+guding.subscriber。original_purchase_date = "2023-02-23T03:33:33Z";
 
-guding.subscriber.subscriptions = {
-  "com.neybox.pillow.premium.month": {
-    "expires_date": "6666-06-06T06:06:06Z",
-    "original_purchase_date": "2023-02-23T02:33:33Z",
-    "purchase_date": "2023-02-23T02:33:33Z",
-    "ownership_type" : "PURCHASED",
+guding.subscriber。subscriptions = {
+  "com.neybox.pillow.premium.year.specialoffer": {
+    "expires_date": "6666-06-06T06:06:06Z"，
+    "original_purchase_date": "2023-02-23T02:33:33Z"，
+    "purchase_date": "2023-02-23T02:33:33Z"，
+    "ownership_type" : "PURCHASED"，
     "store" : "app_store"
   }
 };
