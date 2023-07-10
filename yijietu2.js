5485b71514405c965c6d8df46c20fdab97e21d8a
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-10 13:15:02 +0800

    Update and rename yjt2.js to yijietu2.js

--- yijietu2.js
+++ yijietu2.js
@@ -10,8 +10,8 @@ hostname = jietu6.com, 1jietu.com
 
 */
 var guding = JSON.parse($response.body);
-guding.info.group_id = "3";
-guding.info.vip = "1";
-guding.info.group = "无耻的破解会员";
-guding.info.nickname = "骨钉";
+guding.info。group_id = "3";
+guding.info。vip = "1";
+guding.info。group = "无耻的破解会员";
+guding.info。nickname = "骨钉";
 $done({ body: JSON.stringify(guding) });
