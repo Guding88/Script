2db0971499dff9050f811bb50ec051ac5dfe8e89
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-10 13:12:55 +0800

    Update and rename jjhl.js to jijianhuilv.js

--- jijianhuilv.js
+++ jijianhuilv.js
@@ -1,6 +1,6 @@
 /*
 
-极简汇率
+极简汇率：https://apps.apple.com/app/id851033695
 
 [rewrite_local]
 ^https?:\/\/explorer\.tratao\.com\/api\/client\/xtool\/vip url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/jjhl.js
@@ -11,11 +11,11 @@ hostname = explorer.tratao.com
 */
 var guding = JSON.parse($response.body);
 guding.data = [{
-        "vipStatus": "paid",
-        "vipLevel": "senior",
-        "expire": "2333-02-23 17:11:06",
-        "vipPayType": "auto_sub",
-        "vipPayUnit": "month",
-        "vipPayNum": 1,
+        "vipStatus": "paid"，
+        "vipLevel": "senior"，
+        "expire": "2333-02-23 17:11:06"，
+        "vipPayType": "auto_sub"，
+        "vipPayUnit": "month"，
+        "vipPayNum": 1，
 }]
 $done({ body: JSON.stringify(guding) });
