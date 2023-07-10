29c9befcaa8fe92936a0cfe303f5fc7611a4d796
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-10 13:10:44 +0800

    Update PixelMe.js

--- PixelMe.js
+++ PixelMe.js
@@ -1,27 +1,27 @@
 /*
 
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com\/v1\/subscribers\/[^\/]+$|^https:\/\/api\.revenuecat\.com\/v1\/receipts$ url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/PixelMe.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$)  url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/PixelMe.js
 
 [MITM]
 hostname = api.revenuecat.com
 
 */
 var guding = JSON.parse($response.body);
-guding.subscriber.entitlements = {
+guding.subscriber。entitlements = {
   "pro": {
-    "expires_date": "2333-02-23T02:33:33Z",
-    "product_identifier": "pm_jpy2000_7d0",
+    "expires_date": "2333-02-23T02:33:33Z"，
+    "product_identifier": "pm_jpy2000_7d0"，
     "purchase_date": "6666-06-06T06:06:06Z"
   }
 };
-guding.subscriber.original_purchase_date = "2023-03-13T03:33:33Z";
-guding.subscriber.subscriptions = {
+guding.subscriber。original_purchase_date = "2023-03-13T03:33:33Z";
+guding.subscriber。subscriptions = {
   "pm_jpy2000_7d0": {
-    "expires_date": "6666-06-06T06:06:06Z",
-    "original_purchase_date": "2023-03-13T03:33:33Z",
-    "purchase_date": "2023-02-23T03:33:33Z",
-    "ownership_type" : "PURCHASED",
+    "expires_date": "6666-06-06T06:06:06Z"，
+    "original_purchase_date": "2023-03-13T03:33:33Z"，
+    "purchase_date": "2023-02-23T03:33:33Z"，
+    "ownership_type" : "PURCHASED"，
     "store" : "app_store"
   }
 };
