05c80800c8e7e1ce40abdfacdc352c7b3464c91f
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-10 13:11:14 +0800

    Update Widgetsmith.js

--- Widgetsmith.js
+++ Widgetsmith.js
@@ -1,22 +1,22 @@
 /*
 
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com\/v1\/subscribers\/[^\/]+$|^https:\/\/api\.revenuecat\.com\/v1\/receipts$ url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/Widgetsmith.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$)  url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/Widgetsmith.js
 
 [MITM]
 hostname = api.revenuecat.com
 
 */
 var guding = JSON.parse($response.body);
-guding.subscriber.entitlements = {
+guding.subscriber。entitlements = {
   "Premium": {
-    "expires_date": "2333-02-23T00:02:33Z",
-    "product_identifier": "PremiumAnnualWidget",
+    "expires_date": "2333-02-23T00:02:33Z"，
+    "product_identifier": "PremiumAnnualWidget"，
   }
 };
-guding.subscriber.subscriptions = {
+guding.subscriber。subscriptions = {
   "PremiumAnnualWidget":{
-    "expires_date":"2333-02-23T00:02:33Z",
+    "expires_date":"2333-02-23T00:02:33Z"，
   }
 };
 $done({ body: JSON.stringify(guding) });
