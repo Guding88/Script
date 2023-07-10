33b6638ddc6e14f951c1a7671ce21812505d78df
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-10 13:11:24 +0800

    Update cpp.js

--- cpp.js
+++ cpp.js
@@ -3,25 +3,25 @@
 车票票：https://apps.apple.com/app/id6446212291
 
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com\/v1\/subscribers\/[^\/]+$|^https:\/\/api\.revenuecat\.com\/v1\/receipts$ url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/cpp.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$)  url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/cpp.js
 
 [MITM]
 hostname = api.revenuecat.com
 
 */
 var guding = JSON.parse($response.body);
-guding.subscriber.entitlements = {
+guding.subscriber。entitlements = {
   "vip": {
-    "expires_date": null,
-    "product_identifier": "eticket_6_life",
+    "expires_date": null，
+    "product_identifier": "eticket_6_life"，
     "purchase_date": "2023-03-13T03:33:33Z"
   }
 };
-guding.subscriber.original_purchase_date = "2023-03-13T03:33:33Z";
-guding.subscriber.subscriptions = {
+guding.subscriber。original_purchase_date = "2023-03-13T03:33:33Z";
+guding.subscriber。subscriptions = {
   "eticket_6_life": {
-    "expires_date": null,
-    "original_purchase_date": "2023-03-13T03:33:33Z",
+    "expires_date": null，
+    "original_purchase_date": "2023-03-13T03:33:33Z"，
     "purchase_date": "2023-03-13T03:33:33Z"
   }
 };
