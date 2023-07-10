46c96517d621e8ae05c731c23626d51e850dd631
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-10 13:11:46 +0800

    Update jijianriji.js

--- jijianriji.js
+++ jijianriji.js
@@ -3,27 +3,27 @@
 极简日记：https://apps.apple.com/app/id1568936702
 
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com\/v1\/subscribers\/[^\/]+$|^https:\/\/api\.revenuecat\.com\/v1\/receipts$ url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/jijianriji.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$)  url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/jijianriji.js
 
 [MITM]
 hostname = api.revenuecat.com
 
 */
 var guding = JSON.parse($response.body);
-guding.subscriber.entitlements = {
+guding.subscriber。entitlements = {
   "pro": {
-    "expires_date": null,
-    "product_identifier": "com.mad.MinimalDiary.lifetime",
+    "expires_date": null，
+    "product_identifier": "com.mad.MinimalDiary.lifetime"，
     "purchase_date": "2023-02-23T02:33:33Z"
   }
 };
-guding.subscriber.original_purchase_date = "2023-02-23T03:33:33Z";
-guding.subscriber.subscriptions = {
+guding.subscriber。original_purchase_date = "2023-02-23T03:33:33Z";
+guding.subscriber。subscriptions = {
   "com.mad.MinimalDiary.lifetime": {
-    "expires_date": null,
-    "original_purchase_date": "2023-02-23T02:33:33Z",
-    "purchase_date": "2023-02-23T02:33:33Z",
-    "ownership_type" : "PURCHASED",
+    "expires_date": null，
+    "original_purchase_date": "2023-02-23T02:33:33Z"，
+    "purchase_date": "2023-02-23T02:33:33Z"，
+    "ownership_type" : "PURCHASED"，
     "store" : "app_store"
   }
 };
