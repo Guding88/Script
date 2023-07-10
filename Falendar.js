0ebe2432b0f7c70b36cd299db6d9a3eb1df60cfa
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-10 13:09:54 +0800

    Update Falendar.js

--- Falendar.js
+++ Falendar.js
@@ -1,27 +1,27 @@
 /*
 
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com\/v1\/subscribers\/[^\/]+$|^https:\/\/api\.revenuecat\.com\/v1\/receipts$ url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/Falendar.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$)  url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/Falendar.js
 
 [MITM]
 hostname = api.revenuecat.com
 
 */
 var guding = JSON.parse($response.body);
-guding.subscriber.entitlements = {
+guding.subscriber。entitlements = {
   "Falendar+": {
-    "expires_date": "6666-06-06T06:06:06Z",
-    "product_identifier": "falendar_68_life",
+    "expires_date": "6666-06-06T06:06:06Z"，
+    "product_identifier": "falendar_68_life"，
     "purchase_date": "2023-02-23T02:33:33Z"
   }
 };
-guding.subscriber.original_purchase_date = "2023-02-23T03:33:33Z";
-guding.subscriber.subscriptions = {
+guding.subscriber。original_purchase_date = "2023-02-23T03:33:33Z";
+guding.subscriber。subscriptions = {
   "falendar_68_life": {
-    "expires_date": "6666-06-06T06:06:06Z",
-    "original_purchase_date": "2023-02-23T02:33:33Z",
-    "purchase_date": "2023-02-23T02:33:33Z",
-    "ownership_type" : "PURCHASED",
+    "expires_date": "6666-06-06T06:06:06Z"，
+    "original_purchase_date": "2023-02-23T02:33:33Z"，
+    "purchase_date": "2023-02-23T02:33:33Z"，
+    "ownership_type" : "PURCHASED"，
     "store" : "app_store"
   }
 };
