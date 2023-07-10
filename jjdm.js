7e49efb32f64529f7b7e8276a83ae8e171c0e56c
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-10 13:12:05 +0800

    Update jjdm.js

--- jjdm.js
+++ jjdm.js
@@ -3,22 +3,22 @@
 极简弹幕：https://apps.apple.com/app/id1572801421
 
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com\/v1\/subscribers\/[^\/]+$|^https:\/\/api\.revenuecat\.com\/v1\/receipts$ url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/jjdm.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$)  url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/jjdm.js
 
 [MITM]
 hostname = api.revenuecat.com
 
 */
 var guding = JSON.parse($response.body);
-guding.subscriber.entitlements = {
+guding.subscriber。entitlements = {
   "pro": {
-    "expires_date": "6666-06-06T06:06:06Z",
-    "product_identifier": "com.abouttime.flash.year",
+    "expires_date": "6666-06-06T06:06:06Z"，
+    "product_identifier": "com.abouttime.flash.year"，
   }
 };
-guding.subscriber.subscriptions = {
+guding.subscriber。subscriptions = {
   "com.abouttime.flash.year":{
-    "expires_date":"6666-06-06T06:06:06Z",
+    "expires_date":"6666-06-06T06:06:06Z"，
   }
 };
 $done({ body: JSON.stringify(guding) });
