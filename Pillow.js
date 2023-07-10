e727797c3deebfbddb9b6800f2a5fdac3507ebc6
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-10 10:32:27 +0800

    Create Pillow.js

---
+++ Pillow.js
@@ -0,0 +1,31 @@
+/*
+
+Pillow：https://apps.apple.com/app/id878691772
+
+[rewrite_local]
+^https?:\/\/api\.revenuecat\.com\/v1\/subscribers\/[^\/]+$|^https:\/\/api\.revenuecat\.com\/v1\/receipts$ url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/Pillow.js
+
+[MITM]
+hostname = api.revenuecat.com
+
+*/
+var guding = JSON.parse($response.body);
+guding.subscriber.entitlements = {
+  "premium": {
+    "expires_date": "6666-06-06T06:06:06Z",
+    "product_identifier": "com.neybox.pillow.premium.month",
+    "purchase_date": "2023-02-23T02:33:33Z"
+  }
+};
+guding.subscriber.original_purchase_date = "2023-02-23T03:33:33Z";
+
+guding.subscriber.subscriptions = {
+  "com.neybox.pillow.premium.month": {
+    "expires_date": "6666-06-06T06:06:06Z",
+    "original_purchase_date": "2023-02-23T02:33:33Z",
+    "purchase_date": "2023-02-23T02:33:33Z",
+    "ownership_type" : "PURCHASED",
+    "store" : "app_store"
+  }
+};
+$done({ body: JSON.stringify(guding) });
