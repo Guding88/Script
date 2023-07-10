604639f1a39857a9be70219928e3a4da1df5d8e7
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-10 13:09:23 +0800

    Update Lungy.js

--- Lungy.js
+++ Lungy.js
@@ -3,27 +3,27 @@
 Lungy：https://apps.apple.com/app/id1545223887
 
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com\/v1\/subscribers\/[^\/]+$|^https:\/\/api\.revenuecat\.com\/v1\/receipts$ url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/Lungy.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$)  url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/Lungy.js
 
 [MITM]
 hostname = api.revenuecat.com
 
 */
 var guding = JSON.parse($response.body);
-guding.subscriber.entitlements = {
+guding.subscriber。entitlements = {
   "Deluxe": {
-    "expires_date": "6666-06-06T06:06:06Z",
-    "product_identifier": "lungy_1999_lifetime",
+    "expires_date": "6666-06-06T06:06:06Z"，
+    "product_identifier": "lungy_1999_lifetime"，
     "purchase_date": "2023-02-23T02:33:33Z"
   }
 };
-guding.subscriber.original_purchase_date = "2023-02-23T03:33:33Z";
-guding.subscriber.subscriptions = {
+guding.subscriber。original_purchase_date = "2023-02-23T03:33:33Z";
+guding.subscriber。subscriptions = {
   "lungy_1999_lifetime": {
-    "expires_date": "6666-06-06T06:06:06Z",
-    "original_purchase_date": "2023-02-23T02:33:33Z",
-    "purchase_date": "2023-02-23T02:33:33Z",
-    "ownership_type" : "PURCHASED",
+    "expires_date": "6666-06-06T06:06:06Z"，
+    "original_purchase_date": "2023-02-23T02:33:33Z"，
+    "purchase_date": "2023-02-23T02:33:33Z"，
+    "ownership_type" : "PURCHASED"，
     "store" : "app_store"
   }
 };
