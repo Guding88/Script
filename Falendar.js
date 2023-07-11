4ac5c416650795b4c6ba5b2ba23777cf17f30897
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-11 12:45:17 +0800

    Update Falendar.js

--- Falendar.js
+++ Falendar.js
@@ -10,18 +10,18 @@ hostname = api.revenuecat.com
 var guding = JSON.parse($response.body);
 guding.subscriber.entitlements = {
   "Falendar+": {
-    "expires_date": "6666-06-06T06:06:06Z"，
-    "product_identifier": "falendar_68_life"，
+    "expires_date": "6666-06-06T06:06:06Z",
+    "product_identifier": "falendar_68_life",
     "purchase_date": "2023-02-23T02:33:33Z"
   }
 };
 guding.subscriber.original_purchase_date = "2023-02-23T03:33:33Z";
 guding.subscriber.subscriptions = {
   "falendar_68_life": {
-    "expires_date": "6666-06-06T06:06:06Z"，
-    "original_purchase_date": "2023-02-23T02:33:33Z"，
-    "purchase_date": "2023-02-23T02:33:33Z"，
-    "ownership_type" : "PURCHASED"，
+    "expires_date": "6666-06-06T06:06:06Z",
+    "original_purchase_date": "2023-02-23T02:33:33Z",
+    "purchase_date": "2023-02-23T02:33:33Z",
+    "ownership_type" : "PURCHASED",
     "store" : "app_store"
   }
 };
