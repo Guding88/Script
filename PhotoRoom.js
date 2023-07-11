868fae54b994c2647861eab93635f0f1d155beac
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-11 12:47:48 +0800

    Update PhotoRoom.js

--- PhotoRoom.js
+++ PhotoRoom.js
@@ -12,18 +12,18 @@ hostname = api.revenuecat.com
 var guding = JSON.parse($response.body);
 guding.subscriber.entitlements = {
   "business": {
-    "expires_date": "2333-02-23T02:33:33Z"，
-    "product_identifier": "com.background.business.yearly"，
+    "expires_date": "2333-02-23T02:33:33Z",
+    "product_identifier": "com.background.business.yearly",
     "purchase_date": "2023-02-23T02:33:33Z"
   }
 };
 guding.subscriber.original_purchase_date = "2023-02-23T03:33:33Z";
 guding.subscriber.subscriptions = {
   "com.background.business.yearly": {
-    "expires_date": "6666-06-06T06:06:06Z"，
-    "original_purchase_date": "2023-02-23T02:33:33Z"，
-    "purchase_date": "6666-06-06T06:06:06Z"，
-    "ownership_type" : "PURCHASED"，
+    "expires_date": "6666-06-06T06:06:06Z",
+    "original_purchase_date": "2023-02-23T02:33:33Z",
+    "purchase_date": "6666-06-06T06:06:06Z",
+    "ownership_type" : "PURCHASED",
     "store" : "app_store"
   }
 };
