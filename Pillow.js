fc3c91a95616ea891b1f36b6e9cb1c1508647873
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-11 12:25:11 +0800

    Update Pillow.js

--- Pillow.js
+++ Pillow.js
@@ -10,16 +10,16 @@ hostname = api.revenuecat.com
 
 */
 var guding = JSON.parse($response.body);
-guding.subscriber。entitlements = {
+guding.subscriber.entitlements = {
   "premium": {
     "expires_date": "6666-06-06T06:06:06Z"，
     "product_identifier": "com.neybox.pillow.premium.year.specialoffer"，
     "purchase_date": "2023-02-23T02:33:33Z"
   }
 };
-guding.subscriber。original_purchase_date = "2023-02-23T03:33:33Z";
+guding.subscriber.original_purchase_date = "2023-02-23T03:33:33Z";
 
-guding.subscriber。subscriptions = {
+guding.subscriber.subscriptions = {
   "com.neybox.pillow.premium.year.specialoffer": {
     "expires_date": "6666-06-06T06:06:06Z"，
     "original_purchase_date": "2023-02-23T02:33:33Z"，
