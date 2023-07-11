5872f12fdcde15919caa63a1ba94f87f06d397d4
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-11 12:24:12 +0800

    Update chepiaopiao.js

--- chepiaopiao.js
+++ chepiaopiao.js
@@ -10,15 +10,15 @@ hostname = api.revenuecat.com
 
 */
 var guding = JSON.parse($response.body);
-guding.subscriber。entitlements = {
+guding.subscriber.entitlements = {
   "vip": {
     "expires_date": null，
     "product_identifier": "eticket_6_life"，
     "purchase_date": "2023-03-13T03:33:33Z"
   }
 };
-guding.subscriber。original_purchase_date = "2023-03-13T03:33:33Z";
-guding.subscriber。subscriptions = {
+guding.subscriber.original_purchase_date = "2023-03-13T03:33:33Z";
+guding.subscriber.subscriptions = {
   "eticket_6_life": {
     "expires_date": null，
     "original_purchase_date": "2023-03-13T03:33:33Z"，
