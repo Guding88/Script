ddb7565a70b78c1bf5d7404ec360f45b1a2c46aa
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-11 12:22:39 +0800

    Update shimo.js

--- shimo.js
+++ shimo.js
@@ -10,15 +10,15 @@ hostname = api.revenuecat.com
 
 */
 var guding = JSON.parse($response.body);
-guding.subscriber。entitlements = {
+guding.subscriber.entitlements = {
   "Pro": {
     "expires_date": "6666-06-06T06:06:06Z"，
     "product_identifier": "com.abouttime.todo.year"，
     "purchase_date": "2023-02-23T03:33:33Z"
   }
 };
-guding.subscriber。original_purchase_date = "2023-03-13T03:33:33Z";
-guding.subscriber。subscriptions = {
+guding.subscriber.original_purchase_date = "2023-03-13T03:33:33Z";
+guding.subscriber.subscriptions = {
   "com.abouttime.todo.year": {
     "expires_date": "6666-06-06T06:06:06Z"，
     "original_purchase_date": "2023-03-13T03:33:33Z"，
