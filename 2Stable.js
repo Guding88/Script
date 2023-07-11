9016021fa90ac400506046963a1932cf87087256
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-11 12:44:03 +0800

    Update 2Stable.js

--- 2Stable.js
+++ 2Stable.js
@@ -14,8 +14,8 @@ hostname = api.revenuecat.com
 var guding = JSON.parse($response.body);
 guding.subscriber.entitlements = {
   "PRO": {
-    "expires_date": "6666-06-06T06:06:06Z"，
-    "product_identifier": "2fa_0499_1y"，
+    "expires_date": "6666-06-06T06:06:06Z",
+    "product_identifier": "2fa_0499_1y",
     "purchase_date": "2023-02-23T02:33:33Z"
   }
 };
@@ -23,10 +23,10 @@ guding.subscriber.original_purchase_date = "2023-02-23T03:33:33Z";
 
 guding.subscriber.subscriptions = {
   "2fa_0499_1y": {
-    "expires_date": "6666-06-06T06:06:06Z"，
-    "original_purchase_date": "2023-02-23T02:33:33Z"，
-    "purchase_date": "2023-02-23T02:33:33Z"，
-    "ownership_type" : "PURCHASED"，
+    "expires_date": "6666-06-06T06:06:06Z",
+    "original_purchase_date": "2023-02-23T02:33:33Z",
+    "purchase_date": "2023-02-23T02:33:33Z",
+    "ownership_type" : "PURCHASED",
     "store" : "app_store"
   }
 };
