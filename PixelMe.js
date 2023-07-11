be479addb57f5575722b3858b6f59475b1c8fb41
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-11 12:25:01 +0800

    Update PixelMe.js

--- PixelMe.js
+++ PixelMe.js
@@ -8,15 +8,15 @@ hostname = api.revenuecat.com
 
 */
 var guding = JSON.parse($response.body);
-guding.subscriber。entitlements = {
+guding.subscriber.entitlements = {
   "pro": {
     "expires_date": "2333-02-23T02:33:33Z"，
     "product_identifier": "pm_jpy2000_7d0"，
     "purchase_date": "6666-06-06T06:06:06Z"
   }
 };
-guding.subscriber。original_purchase_date = "2023-03-13T03:33:33Z";
-guding.subscriber。subscriptions = {
+guding.subscriber.original_purchase_date = "2023-03-13T03:33:33Z";
+guding.subscriber.subscriptions = {
   "pm_jpy2000_7d0": {
     "expires_date": "6666-06-06T06:06:06Z"，
     "original_purchase_date": "2023-03-13T03:33:33Z"，
