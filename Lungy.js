d5258ed34b151382a14eb44f050b6e4f13ec59c7
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-11 12:47:08 +0800

    Update Lungy.js

--- Lungy.js
+++ Lungy.js
@@ -12,18 +12,18 @@ hostname = api.revenuecat.com
 var guding = JSON.parse($response.body);
 guding.subscriber.entitlements = {
   "Deluxe": {
-    "expires_date": "6666-06-06T06:06:06Z"，
-    "product_identifier": "lungy_1999_lifetime"，
+    "expires_date": "6666-06-06T06:06:06Z",
+    "product_identifier": "lungy_1999_lifetime",
     "purchase_date": "2023-02-23T02:33:33Z"
   }
 };
 guding.subscriber.original_purchase_date = "2023-02-23T03:33:33Z";
 guding.subscriber.subscriptions = {
   "lungy_1999_lifetime": {
-    "expires_date": "6666-06-06T06:06:06Z"，
-    "original_purchase_date": "2023-02-23T02:33:33Z"，
-    "purchase_date": "2023-02-23T02:33:33Z"，
-    "ownership_type" : "PURCHASED"，
+    "expires_date": "6666-06-06T06:06:06Z",
+    "original_purchase_date": "2023-02-23T02:33:33Z",
+    "purchase_date": "2023-02-23T02:33:33Z",
+    "ownership_type" : "PURCHASED",
     "store" : "app_store"
   }
 };
