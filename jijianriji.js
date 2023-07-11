55288c156c7f37537cf95993150bf736a80249f9
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-11 12:23:25 +0800

    Update jijianriji.js

--- jijianriji.js
+++ jijianriji.js
@@ -10,15 +10,15 @@ hostname = api.revenuecat.com
 
 */
 var guding = JSON.parse($response.body);
-guding.subscriber。entitlements = {
+guding.subscriber.entitlements = {
   "pro": {
     "expires_date": null，
     "product_identifier": "com.mad.MinimalDiary.lifetime"，
     "purchase_date": "2023-02-23T02:33:33Z"
   }
 };
-guding.subscriber。original_purchase_date = "2023-02-23T03:33:33Z";
-guding.subscriber。subscriptions = {
+guding.subscriber.original_purchase_date = "2023-02-23T03:33:33Z";
+guding.subscriber.subscriptions = {
   "com.mad.MinimalDiary.lifetime": {
     "expires_date": null，
     "original_purchase_date": "2023-02-23T02:33:33Z"，
