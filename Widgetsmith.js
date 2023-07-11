3e2b398baf31be4d0072294fc97b4476f7796790
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-11 12:48:48 +0800

    Update Widgetsmith.js

--- Widgetsmith.js
+++ Widgetsmith.js
@@ -10,13 +10,13 @@ hostname = api.revenuecat.com
 var guding = JSON.parse($response.body);
 guding.subscriber.entitlements = {
   "Premium": {
-    "expires_date": "2333-02-23T00:02:33Z"，
-    "product_identifier": "PremiumAnnualWidget"，
+    "expires_date": "2333-02-23T00:02:33Z",
+    "product_identifier": "PremiumAnnualWidget",
   }
 };
 guding.subscriber.subscriptions = {
   "PremiumAnnualWidget":{
-    "expires_date":"2333-02-23T00:02:33Z"，
+    "expires_date":"2333-02-23T00:02:33Z",
   }
 };
 $done({ body: JSON.stringify(guding) });
