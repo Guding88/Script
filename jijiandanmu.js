ab1f239042b8ffeabb2f3390e1f1bd1e81f10d05
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-11 12:49:58 +0800

    Update jijiandanmu.js

--- jijiandanmu.js
+++ jijiandanmu.js
@@ -12,13 +12,13 @@ hostname = api.revenuecat.com
 var guding = JSON.parse($response.body);
 guding.subscriber.entitlements = {
   "pro": {
-    "expires_date": "6666-06-06T06:06:06Z"，
-    "product_identifier": "com.abouttime.flash.year"，
+    "expires_date": "6666-06-06T06:06:06Z",
+    "product_identifier": "com.abouttime.flash.year",
   }
 };
 guding.subscriber.subscriptions = {
   "com.abouttime.flash.year":{
-    "expires_date":"6666-06-06T06:06:06Z"，
+    "expires_date":"6666-06-06T06:06:06Z",
   }
 };
 $done({ body: JSON.stringify(guding) });
