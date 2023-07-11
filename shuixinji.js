3287052fac6b96fd5b671d22befc678f2c69ccc5
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-11 12:51:57 +0800

    Update shuixinji.js

--- shuixinji.js
+++ shuixinji.js
@@ -12,18 +12,18 @@ hostname = api.revenuecat.com
 var guding = JSON.parse($response.body);
 guding.subscriber.entitlements = {
   "pro": {
-    "expires_date": "6666-06-06T06:06:06Z"，
-    "product_identifier": "com.abouttime.heart.year"，
+    "expires_date": "6666-06-06T06:06:06Z",
+    "product_identifier": "com.abouttime.heart.year",
     "purchase_date": "2023-02-23T03:33:33Z"
   }
 };
 guding.subscriber.original_purchase_date = "2023-03-13T03:33:33Z";
 guding.subscriber.subscriptions = {
   "com.abouttime.heart.year": {
-    "expires_date": "6666-06-06T06:06:06Z"，
-    "original_purchase_date": "2023-03-13T03:33:33Z"，
-    "purchase_date": "2023-02-23T03:33:33Z"，
-    "ownership_type" : "PURCHASED"，
+    "expires_date": "6666-06-06T06:06:06Z",
+    "original_purchase_date": "2023-03-13T03:33:33Z",
+    "purchase_date": "2023-02-23T03:33:33Z",
+    "ownership_type" : "PURCHASED",
     "store" : "app_store"
   }
 };
