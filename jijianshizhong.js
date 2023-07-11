37e86a936f9f1c53f259af3907610a7ecd003f56
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-11 12:50:44 +0800

    Update jijianshizhong.js

--- jijianshizhong.js
+++ jijianshizhong.js
@@ -12,8 +12,8 @@ hostname = api.revenuecat.com
 var guding = JSON.parse($response.body);
 guding.subscriber.entitlements = {
   "pro": {
-    "expires_date": "6666-06-06T06:06:06Z"，
-    "product_identifier": "com.mad.zenflipclock.iap.buymeacoffee"，
+    "expires_date": "6666-06-06T06:06:06Z",
+    "product_identifier": "com.mad.zenflipclock.iap.buymeacoffee",
     "purchase_date": "2023-02-23T02:33:33Z"
   }
 };
@@ -21,10 +21,10 @@ guding.subscriber.original_purchase_date = "2023-02-23T03:33:33Z";
 
 guding.subscriber.subscriptions = {
   "com.mad.zenflipclock.iap.buymeacoffee": {
-    "expires_date": "6666-06-06T06:06:06Z"，
-    "original_purchase_date": "2023-02-23T02:33:33Z"，
-    "purchase_date": "2023-02-23T02:33:33Z"，
-    "ownership_type" : "PURCHASED"，
+    "expires_date": "6666-06-06T06:06:06Z",
+    "original_purchase_date": "2023-02-23T02:33:33Z",
+    "purchase_date": "2023-02-23T02:33:33Z",
+    "ownership_type" : "PURCHASED",
     "store" : "app_store"
   }
 };
