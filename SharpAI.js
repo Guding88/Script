38361280b1b650d8191447a54b6818a11ee039fd
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-11 12:33:30 +0800

    更新 SharpAI.js

--- SharpAI.js
+++ SharpAI.js
@@ -12,18 +12,18 @@ hostname = api.revenuecat.com
 var guding = JSON.parse($response.body);
 guding.subscriber.entitlements = {
   "ape.weekly.discount": {
-    "expires_date": "6666-06-06T06:06:06Z"，
-    "product_identifier": "2fa_0499_1y"，
+    "expires_date": "6666-06-06T06:06:06Z",
+    "product_identifier": "2fa_0499_1y",
     "purchase_date": "2023-02-23T02:33:33Z"
   }
 };
 guding.subscriber.original_purchase_date = "2023-02-23T03:33:33Z";
 guding.subscriber.subscriptions = {
   "ape.weekly.discount": {
-    "expires_date": "6666-06-06T06:06:06Z"，
-    "original_purchase_date": "2023-02-23T02:33:33Z"，
-    "purchase_date": "2023-02-23T02:33:33Z"，
-    "ownership_type" : "PURCHASED"，
+    "expires_date": "6666-06-06T06:06:06Z",
+    "original_purchase_date": "2023-02-23T02:33:33Z",
+    "purchase_date": "2023-02-23T02:33:33Z",
+    "ownership_type" : "PURCHASED",
     "store" : "app_store"
   }
 };
