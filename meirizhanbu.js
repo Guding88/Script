9cad8d749c7be48771fb22c4b4a3d34fe84fb827
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-11 17:11:46 +0800

    Create meirizhanbu.js

---
+++ meirizhanbu.js
@@ -0,0 +1,30 @@
+/*
+
+每日占卜：https://apps.apple.com/app/id909048916
+
+[rewrite_local]
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/meirizhanbu.js
+
+[MITM]
+hostname = api.revenuecat.com
+
+*/
+var guding = JSON.parse($response.body);
+guding.subscriber.entitlements = {
+  "Premium": {
+    "expires_date": "6666-06-06T06:06:06Z",
+    "product_identifier": "com.rk.horoscope.month12",
+    "purchase_date": "2023-02-23T02:33:33Z"
+  }
+};
+guding.subscriber.original_purchase_date = "2023-02-23T03:33:33Z";
+guding.subscriber.subscriptions = {
+  "com.rk.horoscope.month12": {
+    "expires_date": "6666-06-06T06:06:06Z",
+    "original_purchase_date": "2023-02-23T02:33:33Z",
+    "purchase_date": "2023-02-23T02:33:33Z",
+    "ownership_type" : "PURCHASED",
+    "store" : "app_store"
+  }
+};
+$done({ body: JSON.stringify(guding) });
