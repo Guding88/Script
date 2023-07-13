b6dc96305e935de40d6eb4798abeedab2a87cc44
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-13 15:07:51 +0800

    Update wodefanqie.js

--- wodefanqie.js
+++ wodefanqie.js
@@ -3,7 +3,7 @@
 我的番茄：https://apps.apple.com/app/id1528322796
 
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com\/v1\/subscribers\/[^\/]+$|^https:\/\/api\.revenuecat\.com\/v1\/receipts$ url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/wodefanqie.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/wodefanqie.js
 
 [MITM]
 hostname = api.revenuecat.com
