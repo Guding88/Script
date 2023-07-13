f736c02e2031e4633bd53b9dd97feb61710ce1cc
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-13 15:06:41 +0800

    Update jijianriji.js

--- jijianriji.js
+++ jijianriji.js
@@ -3,7 +3,7 @@
 极简日记：https://apps.apple.com/app/id1568936702
 
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$)  url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/jijianriji.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/jijianriji.js
 
 [MITM]
 hostname = api.revenuecat.com
