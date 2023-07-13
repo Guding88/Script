70c71dd812d6b24a944b4eb8904d0f6c774b7311
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-13 15:06:49 +0800

    Update jijianshizhong.js

--- jijianshizhong.js
+++ jijianshizhong.js
@@ -3,7 +3,7 @@
 极简时钟：https://apps.apple.com/app/id1265404088
 
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$)  url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/jijianshizhong.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/jijianshizhong.js
 
 [MITM]
 hostname = api.revenuecat.com
