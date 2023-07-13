4a9b0167ed1ca1d234d8e6f81645a2befaff0c0d
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-13 15:06:31 +0800

    Update jijiandanmu.js

--- jijiandanmu.js
+++ jijiandanmu.js
@@ -3,7 +3,7 @@
 极简弹幕：https://apps.apple.com/app/id1572801421
 
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$)  url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/jijiandanmu.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/jijiandanmu.js
 
 [MITM]
 hostname = api.revenuecat.com
