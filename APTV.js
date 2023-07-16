440743f82f289622adbbdcec841d9b17e08d0083
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-16 20:34:53 +0800

    更新 APTV.js

--- APTV.js
+++ APTV.js
@@ -4,7 +4,7 @@ APTV：https://apps.apple.com/app/id1630403500
 
 [rewrite_local]
 ^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/APTV.js
-^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-request https://raw.githubusercontent.com/Guding88/Script/main/APTV.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-request-header https://raw.githubusercontent.com/Guding88/Script/main/APTV.js
 
 [MITM]
 hostname = api.revenuecat.com
