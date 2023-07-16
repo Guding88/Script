ddcad2d5e35c6685591a817e5d4e3875a71a763c
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-16 14:38:51 +0800

    更新 GEIST.js

--- GEIST.js
+++ GEIST.js
@@ -4,35 +4,35 @@ GEIST：https://apps.apple.com/app/id897062509
 
 [rewrite_local]
 ^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/GEIST.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-request https://raw.githubusercontent.com/Guding88/Script/main/GEIST.js
 
 [MITM]
 hostname = api.revenuecat.com
 
 */
-var guding = JSON.parse($response.body);
-guding.subscriber.entitlements = {
-  "memorado_premium": {
-        "grace_period_expires_date": null,
-        "purchase_date": "2023-07-12T15:55:48Z",
-        "product_identifier": "com.memorado.subscription.yearly.v2",
-        "expires_date": "6666-06-06T06:06:06Z"
-      }
-};
-guding.subscriber.original_purchase_date = "2023-07-12T15:55:48Z";
-guding.subscriber.subscriptions = {
-  "com.memorado.subscription.yearly.v2": {
-        "original_purchase_date": "2023-07-12T15:55:48Z",
-        "expires_date": "2023-07-15T15:55:48Z",
-        "is_sandbox": false,
-        "refunded_at": null,
-        "unsubscribe_detected_at": null,
-        "grace_period_expires_date": null,
-        "period_type": "trial",
-        "purchase_date": "2023-07-12T15:55:48Z",
-        "billing_issues_detected_at": null,
-        "ownership_type": "PURCHASED",
-        "store": "app_store",
-        "auto_resume_date": null
-      }
-};
-$done({ body: JSON.stringify(guding) });
+const guding = {};
+const guding6 = JSON.parse(typeof $response != "undefined" && $response.body || null);
+if (typeof $response == "undefined") {
+  delete $request.headers["x-revenuecat-etag"];
+  delete $request.headers["X-RevenueCat-ETag"];
+  guding.headers = $request.headers;
+} else if (guding6 && guding6.subscriber) {
+  guding6.subscriber.subscriptions = guding6.subscriber.subscriptions || {};
+  guding6.subscriber.entitlement = guding6.subscriber.entitlement || {};
+  const app = 'gd';const list = {'gd':{name: 'memorado_premium', id: 'com.memorado.subscription.yearly.v2'}};
+  const data = {
+    "expires_date": "6666-06-06T06:06:06Z",
+    "original_purchase_date": "2023-02-23T02:33:33Z",
+    "purchase_date": "2023-02-23T02:33:33Z",
+    "ownership_type" : "PURCHASED",
+    "store" : "app_store"};
+for (const i in list) { if (new RegExp(`^${i}`, `i`).test(app)) {
+guding6.subscriber.subscriptions[list[i].id] = data;
+guding6.subscriber.entitlements[list[i].name] = JSON.parse(JSON.stringify(data));
+guding6.subscriber.entitlements[list[i].name].product_identifier = list[i].id;
+                break;
+          }
+    }
+    guding.body = JSON.stringify(guding6);
+}
+$done(guding);
