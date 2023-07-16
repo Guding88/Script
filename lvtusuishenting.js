f8925d55e7f6eb64141b0b38476fbe8024a99cfc
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-16 14:15:04 +0800

    更新 lvtusuishenting.js

--- lvtusuishenting.js
+++ lvtusuishenting.js
@@ -3,15 +3,14 @@
 旅途随身听：https://apps.apple.com/app/id1622788638
 
 [rewrite_local]
-^https?:\/\/www\.1314zhilv\.com\/ltsstnew\/user\/getInfo url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/lvtusuishenting.js
-^https?:\/\/www\.1314zhilv\.com\/ltsstnew\/guideScenic\/getScenicAllMarkerList url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/lvtusuishenting.js
+^https?:\/\/www\.1314zhilv\.com\/ltsstnew\/(user\/getInfo|guideScenic\/getScenicAllMarkerList) url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/lvtusuishenting.js
 
 [MITM]
 hostname = www.1314zhilv.com
 
 */
 var guding = $response.body;
-guding = guding.replace(/"isAudition":\d+/g, '"isAudition":1');
+guding = guding.replace(/"isAudition":\d+/g, '"isAudition":2');
 guding = guding.replace(/"identityType":\d+/g, '"identityType":1');
 guding = guding.replace(/"isActivate":\d+/g, '"isActivate":1');
 var guding6 = JSON.parse(guding);
