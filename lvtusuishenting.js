df601f5d926b8ecf4f070d0d2be87311744aaeaa
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-16 11:31:13 +0800

    Update lvtusuishenting.js

--- lvtusuishenting.js
+++ lvtusuishenting.js
@@ -4,17 +4,16 @@
 
 [rewrite_local]
 ^https?:\/\/www\.1314zhilv\.com\/ltsstnew\/user\/getInfo url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/lvtusuishenting.js
-
+^https?:\/\/www\.1314zhilv\.com\/ltsstnew\/guideScenic\/getScenicAllMarkerList url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/lvtusuishenting.js
 
 [MITM]
 hostname = www.1314zhilv.com
 
 */
-var guding = JSON.parse($response.body); 
-guding.content["userId"] = "1"; 
-guding.content["identityType"] = 1; 
-guding.content["headUrl"] = "https://raw.githubusercontent.com/Guding88/iCon/main/Guding.png"; 
-guding.content["isActivate"] = 1; 
-guding.content["levDesc"] = "无耻的破解会员"; 
-guding.content["userName"] = "骨钉"; 
-$done({ body: JSON.stringify(guding) });
+var guding = $response.body;
+guding = guding.replace(/"isAudition":\d+/g, '"isAudition":1');
+guding = guding.replace(/"identityType":\d+/g, '"identityType":1');
+guding = guding.replace(/"isActivate":\d+/g, '"isActivate":1');
+$done({ body:guding});
+
+
