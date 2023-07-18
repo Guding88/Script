a4be8a63c3c5697f25081659acb7c11df6356555
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-18 10:15:58 +0800

    Update iTunes.js

--- iTunes.js
+++ iTunes.js
@@ -32,6 +32,24 @@ for (var key in $request.headers) {
 var UA = $request.headers['user-agent'];
 var guding = JSON.parse($response.body);
 var guding6 = {
+  "receipt": {
+    "receipt_type": "Production",
+    "bundle_id": "com.sugarmo.ScrollClip",
+    "in_app": [
+      {
+        "quantity": "1",
+        "purchase_date_ms": "1669275526000",
+        "transaction_id": "300001282466542",
+        "original_transaction_id": "300001282466542",
+        "product_id": "com.sugarmo.ScrollClip.pro",
+        "in_app_ownership_type": "PURCHASED",
+        "original_purchase_date_ms": "1669275526000"
+      }
+    ],
+    "application_version": "3206",
+    "original_purchase_date_ms": "1669275302000",
+    "original_application_version": "3206",
+  },
   "environment": "Production",
   "pending_renewal_info": [
     {
