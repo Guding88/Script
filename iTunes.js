52686549d49e9a7e68a491d6de3e6a2edd5ba5fe
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-18 15:29:36 +0800

    Update iTunes.js

--- iTunes.js
+++ iTunes.js
@@ -1,6 +1,6 @@
 /*
 
-iTunes解锁合集，已包含APP请见说明：https://github.com/Guding88/Script/blob/main/Readme.md
+iTunes解锁合集，已解锁APP及下载地址请见说明：https://github.com/Guding88/Script/blob/main/Readme.md
 
 [rewrite_local]
 ^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/iTunes.js
