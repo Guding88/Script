1c95edddcb38fd8445e83a92bd19e6873951f072
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-19 17:37:40 +0800

    Update zhijianshiguang.js

--- zhijianshiguang.js
+++ zhijianshiguang.js
@@ -3,12 +3,12 @@
 指尖时光：https://apps.apple.com/app/id1392166974
 
 [rewrite_local]
-^https?:\/\/integral2\.dasyibalang\.com/v2\/User url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/zhijianshiguang.js
+^https?:\/\/integral2\.dasyibalang\.com\/v2\/User url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/zhijianshiguang.js
 
 [MITM]
 hostname = integral2.dasyibalang.com
 
 */
 var guding = $response.body;
-guding  = guding.replace(/"VIP":\d+/g， '"VIP":1');
+guding = guding.replace(/"VIP":\d+/g, '"VIP":1');
 $done({ body:guding});
