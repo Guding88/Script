adb567fcd57af7982602581dd7f11d8c8364a3c0
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-22 12:25:29 +0800

    更新 AllMyBatteries.js

--- AllMyBatteries.js
+++ AllMyBatteries.js
@@ -11,6 +11,24 @@ hostname = buy.itunes.apple.com
 */
 var guding = JSON.parse($response.body);
 guding = {
+  "receipt": {
+    "receipt_type": "Production",
+    "bundle_id": "com.sugarmo.ScrollClip",
+    "in_app": [
+      {
+        "quantity": "1",
+        "purchase_date_ms": "1669275526000",
+        "transaction_id": "300001282466542",
+        "original_transaction_id": "300001282466542",
+        "product_id": "com.sugarmo.ScrollClip.pro",
+        "in_app_ownership_type": "PURCHASED",
+        "original_purchase_date_ms": "1669275526000"
+      }
+    ],
+    "application_version": "3206",
+    "original_purchase_date_ms": "1669275302000",
+    "original_application_version": "3206",
+  },
   "environment": "Production",
   "pending_renewal_info": [
     {
