2d7fe868b7e6bdff34f034ea06230746e1774ce0
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-22 12:19:20 +0800

    更新 APPheji_iTunes.js

--- APPheji_iTunes.js
+++ APPheji_iTunes.js
@@ -141,6 +141,10 @@ if (UA && UA.includes('bazaart')) {
   guding6.latest_receipt_info[0].product_id = "https://t.me/Guding88";
   guding6.pending_renewal_info[0].product_id = "https://t.me/Guding88";
   guding8 = true;
+}else if (UA && UA.includes('AllMyBatteries')) {
+  guding6.latest_receipt_info[0].product_id = "https://t.me/Guding88";
+  guding6.pending_renewal_info[0].product_id = "https://t.me/Guding88";
+  guding8 = true;
 }
 
 //if (guding8) {
