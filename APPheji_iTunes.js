8088d8200430ae6240d630fe515e621d179ebdad
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-24 10:40:48 +0800

    Update APPheji_iTunes.js

--- APPheji_iTunes.js
+++ APPheji_iTunes.js
@@ -145,7 +145,7 @@ if (UA && UA.includes('bazaart')) {
   guding6.latest_receipt_info[0].product_id = "https://t.me/Guding88";
   guding6.pending_renewal_info[0].product_id = "https://t.me/Guding88";
   guding8 = true;
-}else if (UA && UA.includes('Relens')) {
+}else if (UA && UA.includes('ReLens')) {
   guding6.latest_receipt_info[0].product_id = "com.risingcabbage.pro.camera.yearlysubscription";
   guding6.pending_renewal_info[0].product_id = "com.risingcabbage.pro.camera.yearlysubscription";
   guding8 = true;
