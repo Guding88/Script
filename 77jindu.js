7fbe4ee2371027ea7139d6db894eacc046affc64
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-25 11:14:26 +0800

    Create 77jindu.js

---
+++ 77jindu.js
@@ -0,0 +1,17 @@
+/*
+
+77进度：https://apps.apple.com/app/id1660947434
+
+[rewrite_local]
+^https?:\/\/19700\.com\/todo\/api\/v1\/o\/member\/getProfile url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/77jindu.js
+
+[MITM]
+hostname = 19700.com
+
+*/
+var guding = JSON.parse($response.body);
+guding.data.vipEndDate = "6666-06-06 06:06:06";
+guding.data.nickName = "骨钉";
+guding.data.email = "https://t.me/Guding88";
+guding.data.vipFlag = true;
+$done({ body: JSON.stringify(guding) });
