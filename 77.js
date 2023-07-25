fc8daa6e14143542f57a22f18eef9c3d7ddb044e
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-25 11:43:14 +0800

    Update 77.js

--- 77.js
+++ 77.js
@@ -1,6 +1,8 @@
 /*
 
 77进度：https://apps.apple.com/app/id1660947434
+77时钟：https://apps.apple.com/app/id1627747584
+77电脑助手：https://apps.apple.com/app/id1620485227
 
 [rewrite_local]
 ^https?:\/\/19700\.com\/(todo|computer|clock)\/api\/v1\/o\/member\/getProfile url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/77.js
