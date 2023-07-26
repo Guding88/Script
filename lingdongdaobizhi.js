9a3b155c16803992633fe95e03def88a531dc013
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-26 17:20:56 +0800

    Create lingdongdaobizhi.js

---
+++ lingdongdaobizhi.js
@@ -0,0 +1,55 @@
+/*
+
+灵动岛壁纸：https://apps.apple.com/app/id6444463659
+
+[rewrite_local]
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/lingdongdaobizhi.js
+
+[MITM]
+hostname = buy.itunes.apple.com
+
+*/
+var guding = { 
+    "receipt": { 
+      "receipt_type": "Production", 
+      "bundle_id": "https://t.me/Guding88", 
+      "in_app": [ 
+        { 
+          "quantity": "1", 
+          "purchase_date_ms": "666666666666666", 
+          "transaction_id": "666666666666666", 
+          "original_transaction_id": "666666666666666", 
+          "product_id": "islandweek1342", 
+          "in_app_ownership_type": "PURCHASED", 
+          "original_purchase_date_ms": "666666666666666" 
+        } 
+      ], 
+      "application_version": "6", 
+      "original_purchase_date_ms": "666666666666666", 
+      "original_application_version": "6" 
+    }, 
+    "environment": "Production", 
+    "pending_renewal_info": [ 
+      { 
+        "product_id": "islandweek1342", 
+        "auto_renew_status": "1" 
+      } 
+    ], 
+    "status": 0, 
+    "latest_receipt_info": [ 
+      { 
+        "quantity": "1", 
+        "purchase_date_ms": "666666666666666", 
+        "expires_date": "6666-06-06 06:06:06 Etc\/GMT", 
+        "expires_date_pst": "6666-06-06 06:06:06 America\/Los_Angeles", 
+        "is_in_intro_offer_period": "false", 
+        "transaction_id": "666666666666666", 
+        "is_trial_period": "false", 
+        "original_transaction_id": "666666666666666", 
+        "product_id": "islandweek1342", 
+        "original_purchase_date_ms": "666666666666666", 
+        "expires_date_ms": "148204937166000" 
+      } 
+    ] 
+};
+$done({ body: JSON.stringify(guding) });
