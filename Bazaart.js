e342a2ad89ff3c6c7c2536cc91a67bddd8f86ecb
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-26 17:00:41 +0800

    Update Bazaart.js

--- Bazaart.js
+++ Bazaart.js
@@ -9,30 +9,47 @@
 hostname = buy.itunes.apple.com
 
 */
-var guding = JSON.parse($response.body);
-guding = {
-  "environment": "Production",
-  "pending_renewal_info": [
-    {
-      "product_id": "Bazaart_Premium_Monthly_v9",
-      "auto_renew_status": "1"
-    }
-  ],
-  "status": 0,
-  "latest_receipt_info": [
-    {
-      "quantity": "1",
-      "purchase_date_ms": "1688386973000",
-      "expires_date": "6666-06-06 06:06:06 Etc\/GMT",
-      "expires_date_pst": "6666-06-06 06:06:06 America\/Los_Angeles",
-      "is_in_intro_offer_period": "false",
-      "transaction_id": "540001260447637",
-      "is_trial_period": "false",
-      "original_transaction_id": "540001260447637",
-      "product_id": "Bazaart_Premium_Monthly_v9",
-      "original_purchase_date_ms": "1688386974000",
-      "expires_date_ms": "148204937166000"
-    }
-  ]
-}
+var guding = { 
+    "receipt": { 
+      "receipt_type": "Production", 
+      "bundle_id": "https://t.me/Guding88", 
+      "in_app": [ 
+        { 
+          "quantity": "1", 
+          "purchase_date_ms": "666666666666666", 
+          "transaction_id": "666666666666666", 
+          "original_transaction_id": "666666666666666", 
+          "product_id": "Bazaart_Premium_Monthly_v9", 
+          "in_app_ownership_type": "PURCHASED", 
+          "original_purchase_date_ms": "666666666666666" 
+        } 
+      ], 
+      "application_version": "6", 
+      "original_purchase_date_ms": "666666666666666", 
+      "original_application_version": "6" 
+    }, 
+    "environment": "Production", 
+    "pending_renewal_info": [ 
+      { 
+        "product_id": "Bazaart_Premium_Monthly_v9", 
+        "auto_renew_status": "1" 
+      } 
+    ], 
+    "status": 0, 
+    "latest_receipt_info": [ 
+      { 
+        "quantity": "1", 
+        "purchase_date_ms": "666666666666666", 
+        "expires_date": "6666-06-06 06:06:06 Etc\/GMT", 
+        "expires_date_pst": "6666-06-06 06:06:06 America\/Los_Angeles", 
+        "is_in_intro_offer_period": "false", 
+        "transaction_id": "666666666666666", 
+        "is_trial_period": "false", 
+        "original_transaction_id": "666666666666666", 
+        "product_id": "Bazaart_Premium_Monthly_v9", 
+        "original_purchase_date_ms": "666666666666666", 
+        "expires_date_ms": "148204937166000" 
+      } 
+    ] 
+};
 $done({ body: JSON.stringify(guding) });
