74a5bc580c9b79925ad5c5ebea703667114828fe
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-29 00:28:37 +0800

    更新 Focos.js

--- Focos.js
+++ Focos.js
@@ -10,6 +10,27 @@ hostname = focos.oracle.bendingspoonsapps.com
 
 */
 var guding = JSON.parse($response.body);
-guding.me.active_subscriptions_ids = ["com.focos.1y_t130_1w_bundle_creator"];
-guding.me.unique_id = "88888888-8888-8888-8888-888888888888";
+//guding.me.active_subscriptions_ids = ["com.focos.1y_t130_1w_bundle_creator"];
+guding.me = {
+    "non_consumables_ids": [
+    ],
+    "active_subscriptions_ids": [
+      "com.focos.1y_t130_1w_bundle_creator"
+    ],
+    "privacy_notice": {
+      "required_consents": [
+      ],
+      "is_at_least_16": "unknown",
+      "last_acknowledged_version": "1.1.0"
+    },
+    "unique_id": "88888888-8888-8888-8888-888888888888",
+    "active_bundle_subscriptions": [
+    ],
+    "is_spooner": false,
+    "terms_of_service": {
+      "last_accepted_version": "1.8.0"
+    },
+    "available_consumable_credits": {
+    }
+  };
 $done({ body: JSON.stringify(guding) });
