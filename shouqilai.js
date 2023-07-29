4e059ef67040182a305cf46b02b700f6f93ed418
Guding <132737252+Guding88@users.noreply.github.com>
2023-07-29 11:43:31 +0800

    Create shouqilai.js

---
+++ shouqilai.js
@@ -0,0 +1,11 @@
+/*
+
+收起来：https://apps.apple.com/app/id1669206548
+
+[rewrite_local]
+^https?:\/\/api\.shouqilai\.cn\/m\/receive\/account\/getAccountByToken url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/shouqilai.js
+
+[MITM]
+hostname = api.shouqilai.cn
+
+*/
