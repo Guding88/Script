d257757202b14262a36a62d1eb82bf7e6b13fa7c
Guding <132737252+Guding88@users.noreply.github.com>
2023-08-01 15:25:01 +0800

    Create xiongzhangji.js

---
+++ xiongzhangji.js
@@ -0,0 +1,58 @@
+/*
+
+熊掌记：https://apps.apple.com/app/id1016366447
+
+[rewrite_local]
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/xiongzhangji.js
+
+[MITM]
+hostname = buy.itunes.apple.com
+
+*/
+var guding = { 
+    "receipt": { 
+      "receipt_type": "Production", 
+      "bundle_id": "https://t.me/Guding88", 
+      "in_app": [ 
+        { 
+          "quantity": "1", 
+          "expires_date": "6666-06-06 06:06:06 Etc\/GMT",
+          "expires_date_pst": "6666-06-06 06:06:06 America\/Los_Angeles",
+          "purchase_date_ms": "666666666666666", 
+          "transaction_id": "666666666666666", 
+          "original_transaction_id": "666666666666666", 
+          "product_id": "net.shinyfrog.bear_iOS.pro_yearly_subscription_bis", 
+          "in_app_ownership_type": "PURCHASED", 
+          "original_purchase_date_ms": "666666666666666" 
+        } 
+      ], 
+      "request_date": "2022-02-02 06:06:06 Etc\/GMT",
+      "application_version": "6", 
+      "original_purchase_date_ms": "666666666666666", 
+      "original_application_version": "6" 
+    }, 
+    "environment": "Production", 
+    "pending_renewal_info": [ 
+      { 
+        "product_id": "net.shinyfrog.bear_iOS.pro_yearly_subscription_bis", 
+        "auto_renew_status": "1" 
+      } 
+    ], 
+    "status": 0, 
+    "latest_receipt_info": [ 
+      { 
+        "quantity": "1", 
+        "purchase_date_ms": "666666666666666", 
+        "expires_date": "6666-06-06 06:06:06 Etc\/GMT", 
+        "expires_date_pst": "6666-06-06 06:06:06 America\/Los_Angeles", 
+        "is_in_intro_offer_period": "false", 
+        "transaction_id": "666666666666666", 
+        "is_trial_period": "false", 
+        "original_transaction_id": "666666666666666", 
+        "product_id": "net.shinyfrog.bear_iOS.pro_yearly_subscription_bis", 
+        "original_purchase_date_ms": "666666666666666", 
+        "expires_date_ms": "148204937166000" 
+      } 
+    ] 
+};
+$done({ body: JSON.stringify(guding) });
