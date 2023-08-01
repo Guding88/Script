ee612b784cf0729bf4e3e2be641517fef9c70669
Guding <132737252+Guding88@users.noreply.github.com>
2023-08-01 15:02:07 +0800

    Update APPheji_Revenuecat.js

--- APPheji_Revenuecat.js
+++ APPheji_Revenuecat.js
@@ -116,6 +116,7 @@ if (typeof $response == "undefined") {
     'Thiro':{ name: 'pro', id: 'atelerix_pro_lifetime'},
     'FTChatAI':{ name: 'pro', id: 'ft_ai_1799_1y'},
     '%E7%A7%A9%E5%BA%8F%E7%9B%AE%E6%A0%87':{ name: 'pro', id: 'com.metaorder.OKRTomato.vip.supremacy'},
+    'Zoomerang':{ name: 'pro', id: 'zoomerang.yearly'},
 
 
     
