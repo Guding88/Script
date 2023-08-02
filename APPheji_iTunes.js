c64dffc5071604fd00961a8ab2bc4ed40879348d
Guding <132737252+Guding88@users.noreply.github.com>
2023-08-02 19:01:01 +0800

    Update APPheji_iTunes.js

--- APPheji_iTunes.js
+++ APPheji_iTunes.js
@@ -60,51 +60,75 @@ var uaProductMapping = {
 
 
 };
-var guding6 = { 
-    "receipt": { 
-      "receipt_type": "Production", 
-      "bundle_id": "https://t.me/Guding88", 
-      "in_app": [ 
-        { 
-          "quantity": "1", 
-          "expires_date": "6666-06-06 06:06:06 Etc\/GMT",
-          "expires_date_pst": "6666-06-06 06:06:06 America\/Los_Angeles",
-          "purchase_date_ms": "666666666666666", 
-          "transaction_id": "666666666666666", 
-          "original_transaction_id": "666666666666666", 
-          "product_id": "https://t.me/Guding88", 
-          "in_app_ownership_type": "PURCHASED", 
-          "original_purchase_date_ms": "666666666666666" 
-        } 
-      ], 
-      "request_date": "2022-02-02 06:06:06 Etc\/GMT",
-      "application_version": "6", 
-      "original_purchase_date_ms": "666666666666666", 
-      "original_application_version": "6" 
-    }, 
-    "environment": "Production", 
-    "pending_renewal_info": [ 
-      { 
-        "product_id": "https://t.me/Guding88", 
-        "auto_renew_status": "1" 
-      } 
-    ], 
-    "status": 0, 
-    "latest_receipt_info": [ 
-      { 
-        "quantity": "1", 
-        "purchase_date_ms": "666666666666666", 
-        "expires_date": "6666-06-06 06:06:06 Etc\/GMT", 
-        "expires_date_pst": "6666-06-06 06:06:06 America\/Los_Angeles", 
-        "is_in_intro_offer_period": "false", 
-        "transaction_id": "666666666666666", 
-        "is_trial_period": "false", 
-        "original_transaction_id": "666666666666666", 
-        "product_id": "https://t.me/Guding88", 
-        "original_purchase_date_ms": "666666666666666", 
-        "expires_date_ms": "148204937166000" 
-      } 
-    ] 
+var guding6 = {
+  "environment": "Production",
+  "receipt": {
+    "receipt_type": "Production",
+    "receipt_creation_date": "2022-02-02 06:06:06 Etc\/GMT",
+    "bundle_id": "https://t.me/Guding88",
+    "original_purchase_date": "2022-02-02 06:06:06 Etc\/GMT",
+    "in_app": [
+      {
+        "quantity": "1",
+        "purchase_date_ms": "1643753166000",
+        "expires_date": "6666-06-06 06:06:06 Etc\/GMT",
+        "expires_date_pst": "6666-06-06 06:06:06 America\/Los_Angeles",
+        "is_in_intro_offer_period": "false",
+        "transaction_id": "666666666666666",
+        "is_trial_period": "false",
+        "original_transaction_id": "666666666666666",
+        "purchase_date": "2022-02-02 06:06:06 Etc\/GMT",
+        "product_id": "https://t.me/Guding88",
+        "original_purchase_date_pst": "2022-02-02 06:06:06 America\/Los_Angeles",
+        "in_app_ownership_type": "PURCHASED",
+        "original_purchase_date_ms": "1643753166000",
+        "web_order_line_item_id": "666666666666666",
+        "expires_date_ms": "148204937166000",
+        "purchase_date_pst": "2022-02-02 06:06:06 America\/Los_Angeles",
+        "original_purchase_date": "2022-02-02 06:06:06 Etc\/GMT"
+      }
+    ],
+    "receipt_creation_date_pst": "2022-02-02 06:06:06 America\/Los_Angeles",
+    "request_date": "2022-02-02 06:06:06 Etc\/GMT",
+    "request_date_pst": "2022-02-02 06:06:06 America\/Los_Angeles",
+    "request_date_ms": "1643753166000",
+    "original_purchase_date_pst": "2022-02-02 06:06:06 America\/Los_Angeles",
+    "application_version": "123",
+    "original_purchase_date_ms": "1643753166000",
+    "receipt_creation_date_ms": "1643753166000",
+    "original_application_version": "123",
+  },
+  "pending_renewal_info": [
+    {
+      "product_id": "https://t.me/Guding88",
+      "original_transaction_id": "666666666666666",
+      "auto_renew_product_id": "https://t.me/Guding88",
+      "auto_renew_status": "1"
+    }
+  ],
+  "status": 0,
+  "latest_receipt_info": [
+    {
+      "quantity": "1",
+      "purchase_date_ms": "1643753166000",
+      "expires_date": "6666-06-06 06:06:06 Etc\/GMT",
+      "expires_date_pst": "6666-06-06 06:06:06 America\/Los_Angeles",
+      "is_in_intro_offer_period": "false",
+      "transaction_id": "666666666666666",
+      "is_trial_period": "false",
+      "original_transaction_id": "666666666666666",
+      "purchase_date": "2022-02-02 06:06:06 Etc\/GMT",
+      "product_id": "https://t.me/Guding88",
+      "original_purchase_date_pst": "2022-02-02 06:06:06 America\/Los_Angeles",
+      "in_app_ownership_type": "PURCHASED",
+      "subscription_group_identifier": "66666666",
+      "original_purchase_date_ms": "1643753166000",
+      "web_order_line_item_id": "666666666666666",
+      "expires_date_ms": "148204937166000",
+      "purchase_date_pst": "2022-02-02 06:06:06 America\/Los_Angeles",
+      "original_purchase_date": "2022-02-02 06:06:06 Etc\/GMT"
+    }
+  ]
 };
 var guding8 = false;
 for (var uaKey in uaProductMapping) {
