8c7184a554879988b6f707a3f6c1d09d66f5ac1f
Guding <132737252+Guding88@users.noreply.github.com>
2023-08-02 18:20:25 +0800

    Create ‎Athlytic.js

---
+++ ‎Athlytic.js
@@ -0,0 +1,58 @@
+/*
+
+Athlytic：https://apps.apple.com/app/id515094775
+
+[rewrite_local]
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/Athlytic.js
+
+[MITM]
+hostname = buy.itunes.apple.com
+
+*/
+var guding = { 
+    "receipt": { 
+      "receipt_type": "Production", 
+      "bundle_id": "myndarc.newFitnessApp", 
+      "in_app": [ 
+        { 
+          "quantity": "1", 
+          "expires_date": "6666-06-06 06:06:06 Etc\/GMT",
+          "expires_date_pst": "6666-06-06 06:06:06 America\/Los_Angeles",
+          "purchase_date_ms": "666666666666666", 
+          "transaction_id": "666666666666666", 
+          "original_transaction_id": "666666666666666", 
+          "product_id": "newfitnessapp02", 
+          "in_app_ownership_type": "PURCHASED", 
+          "original_purchase_date_ms": "666666666666666" 
+        } 
+      ], 
+      "request_date": "2022-02-02 06:06:06 Etc\/GMT",
+      "application_version": "6", 
+      "original_purchase_date_ms": "666666666666666", 
+      "original_application_version": "6" 
+    }, 
+    "environment": "Production", 
+    "pending_renewal_info": [ 
+      { 
+        "product_id": "newfitnessapp02", 
+        "auto_renew_status": "1" 
+      } 
+    ], 
+    "status": 0, 
+    "latest_receipt_info": [ 
+      { 
+        "quantity": "1", 
+        "purchase_date_ms": "666666666666666", 
+        "expires_date": "6666-06-06 06:06:06 Etc\/GMT", 
+        "expires_date_pst": "6666-06-06 06:06:06 America\/Los_Angeles", 
+        "is_in_intro_offer_period": "false", 
+        "transaction_id": "666666666666666", 
+        "is_trial_period": "false", 
+        "original_transaction_id": "666666666666666", 
+        "product_id": "newfitnessapp02", 
+        "original_purchase_date_ms": "666666666666666", 
+        "expires_date_ms": "148204937166000" 
+      } 
+    ] 
+};
+$done({ body: JSON.stringify(guding) });
