bb7bc7e90f500c232dad25e07085b19d0d67d9bf
Guding <132737252+Guding88@users.noreply.github.com>
2023-08-03 17:07:31 +0800

    Create Agenda.js

---
+++ Agenda.js
@@ -0,0 +1,25 @@
+/*
+
+Agenda：https://apps.apple.com/app/id1370289240
+
+[rewrite_local]
+^https?:\/\/accounts\.agenda\.com\/users url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/Agenda.js
+
+[MITM]
+hostname = accounts.agenda.com
+
+*/
+var guding = {
+ "unlockExpiryMobile": 148204937166000,
+  "licenseStatusMobile": 0,
+  "licenseStatus": 2,
+  "informUserOfPendingBonus": false,
+  "hasLifetimeUnlock": true,
+  "freeTrialStatus": 0,
+  "isSubscribed": true,
+  "unlockExpiry": 148204937166000,
+  "isSubscribedMobile": true,
+  "error": 0,
+  "universalPurchaseAvailable": false
+}
+$done({ body: JSON.stringify(guding) });
