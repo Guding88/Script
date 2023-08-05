3a3a95375f5be39ff33fd44027270953bdd20969
Guding <132737252+Guding88@users.noreply.github.com>
2023-08-05 23:43:44 +0800

    更新 TouchRetouch.js

--- TouchRetouch.js
+++ TouchRetouch.js
@@ -3,8 +3,8 @@
 ‎TouchRetouch：https://apps.apple.com/app/id373311252
 
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/‎TouchRetouch.js
-^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-request-header https://raw.githubusercontent.com/Guding88/Script/main/‎TouchRetouch.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/TouchRetouch.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-request-header https://raw.githubusercontent.com/Guding88/Script/main/TouchRetouch.js
 
 [MITM]
 hostname = api.revenuecat.com
