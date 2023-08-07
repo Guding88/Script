da88eef25bf4bcaf8d67f246ae698886ef27c69c
Guding <132737252+Guding88@users.noreply.github.com>
2023-08-07 14:35:06 +0800

    Update AmiiBoss.js

--- AmiiBoss.js
+++ AmiiBoss.js
@@ -18,21 +18,38 @@ if (typeof $response == "undefined") {
   guding.headers = $request.headers;
 } else if (guding6 && guding6.subscriber) {
   guding6.subscriber.subscriptions = guding6.subscriber.subscriptions || {};
-  guding6.subscriber.entitlement = guding6.subscriber.entitlement || {};
-  const app = 'gd';const list = {'gd':{name: 'pro', id: 'amiiboss_pro'}};
+  guding6.subscriber.entitlements = guding6.subscriber.entitlements || {};
+  var headers = {};
+  for (var key in $request.headers) {
+  const reg = /^[a-z]+$/;
+  if (key === "User-Agent" && !reg.test(key)) {
+    var lowerkey = key.toLowerCase();
+    $request.headers[lowerkey] = $request.headers[key];
+    delete $request.headers[key];
+    }
+  }
+  var UA = $request.headers['user-agent'];
+  const app = 'gd';
+  const UAMappings = {
+    'AmiiBoss':{ name: 'pro', id: 'amiiboss_pro'},
+  };
   const data = {
     "expires_date": "6666-06-06T06:06:06Z",
     "original_purchase_date": "2023-02-23T02:33:33Z",
     "purchase_date": "2023-02-23T02:33:33Z",
-    "ownership_type" : "PURCHASED",
-    "store" : "app_store"};
-for (const i in list) { if (new RegExp(`^${i}`, `i`).test(app)) {
-guding6.subscriber.subscriptions[list[i].id] = data;
-guding6.subscriber.entitlements[list[i].name] = JSON.parse(JSON.stringify(data));
-guding6.subscriber.entitlements[list[i].name].product_identifier = list[i].id;
-                break;
-          }
+    "ownership_type": "PURCHASED",
+    "store": "app_store"
+  };
+  for (const i in UAMappings) {
+    if (new RegExp(`^${i}`, 'i').test(UA)) {
+      const { name, id } = UAMappings[i];
+      guding6.subscriber.subscriptions = {};
+      guding6.subscriber.subscriptions[id] = data;
+      guding6.subscriber.entitlements[name] = JSON.parse(JSON.stringify(data));
+      guding6.subscriber.entitlements[name].product_identifier = id;
+      break;
     }
-    guding.body = JSON.stringify(guding6);
+  }
+  guding.body = JSON.stringify(guding6);
 }
 $done(guding);
