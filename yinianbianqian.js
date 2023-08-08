6760bf5ec1038a3b58201bff76a4e37445bd2dc2
Guding <132737252+Guding88@users.noreply.github.com>
2023-08-08 15:21:41 +0800

    Update yinianbianqian.js

--- yinianbianqian.js
+++ yinianbianqian.js
@@ -1,15 +1,4 @@
-/*
 
 一念便签：https://apps.apple.com/app/id1473578864
 
-[rewrite_local]
-^https?:\/\/yinian\.pro\/api\/user url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/yinianbianqian.js
-
-[MITM]
-hostname = yinian.pro
-
-*/
-var guding = JSON.parse($response.body);
-guding.data.is_vip = 1;
-guding.data.vip_end_time = "6666-06-06 06:06:06";
-$done({ body: JSON.stringify(guding) });
+如需使用会员功能，请在APP内购买使用，感谢您的支持。
