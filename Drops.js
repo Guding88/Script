bd1872cac0a43f1577e88cf94d27d34bf85a5a76
Guding <132737252+Guding88@users.noreply.github.com>
2023-08-11 21:11:08 +0800

    更新 Drops.js

--- Drops.js
+++ Drops.js
@@ -3,8 +3,8 @@
 ‎Drops：https://apps.apple.com/app/id939540371
 
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/‎Drops.js
-^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-request-header https://raw.githubusercontent.com/Guding88/Script/main/‎Drops.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/Drops.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-request-header https://raw.githubusercontent.com/Guding88/Script/main/Drops.js
 
 [MITM]
 hostname = api.revenuecat.com
