80733492f518f313e5c2df11bbcdceb70d050f16
Guding <132737252+Guding88@users.noreply.github.com>
2023-08-16 17:00:42 +0800

    Update and rename ‎Noverdue.js to Noverdue.js

--- Noverdue.js
+++ Noverdue.js
@@ -3,8 +3,8 @@
 ‎Noverdue：https://apps.apple.com/app/id1492210816
 
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/‎Noverdue.js
-^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-request-header https://raw.githubusercontent.com/Guding88/Script/main/‎Noverdue.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/Noverdue.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-request-header https://raw.githubusercontent.com/Guding88/Script/main/Noverdue.js
 
 [MITM]
 hostname = api.revenuecat.com
