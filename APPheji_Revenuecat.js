f51d7e1498e9b333a9b19773225288bc530659ce
Guding <132737252+Guding88@users.noreply.github.com>
2023-08-23 14:58:04 +0800

    Update APPheji_Revenuecat.js

--- APPheji_Revenuecat.js
+++ APPheji_Revenuecat.js
@@ -139,7 +139,6 @@ if (typeof $response == "undefined") {
     'ImAFish':{ name: 'Pro', id: 'ProOver'},
     'GradientMusic':{ name: 'Pro', id: 'com.gradient.vision.new.music.one.time.79'},
     'streaks':{ name: 'patron', id: 'com.andyworks.streaks.yearlyPatron'},
-    'Grow':{ name: 'grow.pro', id: 'grow_lifetime'},
 
 
     
