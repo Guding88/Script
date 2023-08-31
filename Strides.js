c980d0061793f2373e444763beaeecf6c1f41b34
Guding <132737252+Guding88@users.noreply.github.com>
2023-08-31 10:47:51 +0800

    Update Strides.js

--- Strides.js
+++ Strides.js
@@ -1,6 +1,6 @@
 /*
 
-Strides：https://apps.apple.com/app/id1579972834
+Strides：https://apps.apple.com/app/id672401817
 
 [rewrite_local]
 ^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/Strides.js
