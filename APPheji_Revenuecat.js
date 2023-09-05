f20ba37c46ca1d85ed512f572d7ba35cdcbaa216
Guding <132737252+Guding88@users.noreply.github.com>
2023-09-05 10:38:32 +0800

    Update APPheji_Revenuecat.js

--- APPheji_Revenuecat.js
+++ APPheji_Revenuecat.js
@@ -144,6 +144,7 @@ if (typeof $response == "undefined") {
     'Summit_iOS':{ name: 'pro', id: 'Summit_Monthly1'},
     'PomoStiker':{ name: 'lifetimeForever', id: 'com.daya.lifetimeforever'},
     'Uio':{ name: 'PRO', id: 'com.daya.Uio.quarterly'},
+    'Snipd':{ name: 'premium', id: 'snipd_premium_1y_7199_trial_2w_v2'},
 
 
     
