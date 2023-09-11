4ad33ef0f24f228824d99058aeb6c7bae6058f7c
Guding <132737252+Guding88@users.noreply.github.com>
2023-09-11 15:40:19 +0800

    Update Metion.js

--- Metion.js
+++ Metion.js
@@ -3,7 +3,7 @@
 Metion：https://apps.apple.com/app/id1530965242
 
 [rewrite_local]
-^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/Bazaart.js
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/Metion.js
 
 [MITM]
 hostname = buy.itunes.apple.com
