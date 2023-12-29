20a6d8bea93ed96c405924cef8f0b74450c76ee8
Guding <132737252+Guding88@users.noreply.github.com>
2023-12-29 09:47:29 +0800

    Update APPheji_Revenuecat.js

--- APPheji_Revenuecat.js
+++ APPheji_Revenuecat.js
@@ -149,11 +149,11 @@ if (typeof $response == "undefined") {
     'CountDuck':{ name: 'premium', id: 'Lifetime'},
     'tiimo':{ name: 'full_access', id: 'lifetime.iap'},
     'Subtrack':{ name: 'pro', id: 'com.mohitnandwani.subtrack.subtrackpro.family'},
-    'Aphrodite':{ name: 'all', id: 'com.ziheng.aphrodite.onetime'},
+    'Aphrodite':{ name: 'all', id: 'com.ziheng.aphrodite.lifetime'},
     '%E7%94%BB%E5%8E%86':{ name: 'pro', id: 'com.bapaws.calendar.lifetime'},
     'NotSmoke':{ name: 'not_smoke_vip_01', id: 'not_smoke_01_12_month'},
     'Record2Text':{ name: 'afi.recnote.pro', id: 'afi.recnote.pro'},
-		'MusicMate':{ name: 'premium', id: 'mm_lifetime_198_premium'},
+    'MusicMate':{ name: 'premium', id: 'mm_lifetime_198_premium'},
 
 
     
