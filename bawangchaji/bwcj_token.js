04d817a434d5eb0671e3a1cab85086ce4ef06acb
Guding <132737252+Guding88@users.noreply.github.com>
2024-01-09 15:27:20 +0800

    Update bwcj_token.js

--- bawangchaji/bwcj_token.js
+++ bawangchaji/bwcj_token.js
@@ -6,7 +6,7 @@
     用户信息获取成功后可禁用此脚本
 
 [rewrite local]
-^https?:\/\/webapi\.qmai\.cn\/web\/catering\/integral\/sign\/signIn url script-request-body https://raw.githubusercontent.com/Guding88/Script/main/bawangchaji/bwcj_token.js
+//^https?:\/\/webapi\.qmai\.cn\/web\/catering\/integral\/sign\/signIn url script-request-body https://raw.githubusercontent.com/Guding88/Script/main/bawangchaji/bwcj_token.js
 ^https?:\/\/webapi\.qmai\.cn\/web\/catering\/user-cover-image\/queryUserCoverImage url script-request-body https://raw.githubusercontent.com/Guding88/Script/main/bawangchaji/bwcj_token.js
 
 [MITM]
