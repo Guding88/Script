24196368cc1caf8fefba7f24ae1337f872e4016c
Guding <132737252+Guding88@users.noreply.github.com>
2024-02-01 17:29:43 +0800

    Update feiyujihua.js

--- feiyujihua.js
+++ feiyujihua.js
@@ -3,10 +3,10 @@
 飞鱼计划：https://apps.apple.com/app/id1571229028
 
 [rewrite_local]
-^https?:\/\/getway-plan\.abuddz\.com\/api\/sync\/putNeedUpdateData url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/feiyujihua.js
+^https?:\/\/getway-plan\.bjsybw\.cn\/api\/sync\/putNeedUpdateData url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/feiyujihua.js
 
 [MITM]
-hostname = getway-plan.abuddz.com
+hostname = getway-plan.bjsybw.cn
 
 */
 var guding = JSON.parse($response.body);
