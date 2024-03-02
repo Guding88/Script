6b4c8c0c45dffeb15fc6cf11f67bf769a7d2e125
Guding <132737252+Guding88@users.noreply.github.com>
2024-03-02 20:19:21 +0800

    Update boshu.js

--- boshu.js
+++ boshu.js
@@ -3,10 +3,10 @@
 博树：https://apps.apple.com/app/id379968583
 
 [rewrite_local]
-^https?:\/\/api\.busuu\.com\/users\/me url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/boshu.js
+^https?:\/\/api\.(busuu|boshu)\.(com|cn)\/users\/me url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/boshu.js
 
 [MITM]
-hostname = api.busuu.com
+hostname = api.busuu.com, api.boshu.cn
 
 */
 var guding = JSON.parse($response.body);
